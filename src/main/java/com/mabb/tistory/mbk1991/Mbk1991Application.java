7b590652be67a4e7055081d07fa56de98aecfc0c
mbk1991 <mbk1991@next-core.co.kr>
2025-05-18 15:07:25 +0900

    Add Spring Boot skeleton

---
+++ src/main/java/com/mabb/tistory/mbk1991/Mbk1991Application.java
@@ -0,0 +1,13 @@
+package com.mabb.tistory.mbk1991;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mbk1991Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mbk1991Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mabb/tistory/mbk1991/Mbk1991ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabb.tistory.mbk1991;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mbk1991ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
